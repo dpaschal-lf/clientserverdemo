566e38481c73a32f5a4a1765f087d672771d3e1f
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-08-06 22:19:14 +0000

    store, read, and display data added data to server sent data through endpoints changd client address to correct server address put names on dom

--- server/server.js
+++ server/server.js
@@ -1,3 +1,37 @@
 
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+const names = [{
+	"name": "Samantha"
+}, {
+	"name": "Jae"
+}, {
+	"name": "Nicholaus"
+}];
+
+const favFoods = [{
+	"foodName": "salmon",
+	"agePreferred": 12
+}, {
+	"foodName": "human souls",
+	"agePreferred": 30
+}];
+
+server.use( cors() );
+
+server.get('/favfoods', (request, response)=>{
+    response.send(favFoods);
+})
+
+server.get('/names', (request, response)=>{
+    response.send(names);
+})
+
+server.listen(3001, ()=>{
+    console.log('carrier has arrived');
+})
+
 
 
